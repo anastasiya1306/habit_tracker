e6eeb4c582af2f8b8d3d56528e84c7e1c64e81ae
Anastasiya Burykh <burykh_2000@mail.ru>
2023-08-31 16:13:19 +0300

    add urls

--- users/urls.py
+++ users/urls.py
@@ -10,9 +10,9 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/', UserListView.as_view(), name='users_list'),
-    path('users/create/', UserCreateView.as_view(), name='users_create'),
-    path('users/detail/<int:pk>/', UserRetrieveView.as_view(), name='users_detail'),
-    path('users/update/<int:pk>/', UserUpdateView.as_view(), name='users_update'),
-    path('users/delete/<int:pk>/', UserDestroyView.as_view(), name='users_delete')
+    path('', UserListView.as_view(), name='users_list'),
+    path('create/', UserCreateView.as_view(), name='users_create'),
+    path('detail/<int:pk>/', UserRetrieveView.as_view(), name='users_detail'),
+    path('update/<int:pk>/', UserUpdateView.as_view(), name='users_update'),
+    path('delete/<int:pk>/', UserDestroyView.as_view(), name='users_delete')
 ]
