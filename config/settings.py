f11fc3f143b0edce67d446c6b182a529bce78b49
Anastasiya Burykh <burykh_2000@mail.ru>
2023-12-13 18:21:52 +0300

    add Dockerfile

--- config/settings.py
+++ config/settings.py
@@ -93,24 +93,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'habit_tracker_project',
-#        'USER': 'postgres',
-#        'PASSWORD': 'simba2106',
-#        'HOST': 'db',
-#        'PORT': '5432'
-#    }
-#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT')
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 # Password validation
